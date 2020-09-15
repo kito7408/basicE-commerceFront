c9d864f256b6b98bf00c802f804f2c7e790236df
Cristopher <Cristopher.lizandro.11@gmail.com>
2020-09-15 01:34:08 -0500

    changes for heroku 3

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/test-sonr'));
+app.use(express.static(__dirname + '/test-sonr'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/test-sonr/index.html'));});
-app.listen(process.env.PORT || 8080);
+'/test-sonr/index.html'));});
+app.listen(process.env.PORT || 8080, () => {
+	console.log('server on port 3000');
+});
