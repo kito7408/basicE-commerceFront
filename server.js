31e02c0a3efedcc9d2558a84b99eab36ee0783a9
Cristopher <Cristopher.lizandro.11@gmail.com>
2020-09-15 01:25:27 -0500

    changes for heroku 2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/test-sonr'));
+app.use(express.static(__dirname + '/dist/test-sonr'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/test-sonr/index.html'));});
+'/dist/test-sonr/index.html'));});
 app.listen(process.env.PORT || 8080);
